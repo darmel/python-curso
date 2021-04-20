9e8a631dc6b5251316d77cc1a5eaff8df80722f6
dario.ochoa <dario.ochoa@incluit.com>
2021-04-20 17:42:08 -0300

    add diccionarios y tuplas

---
+++ diccionarios.py
@@ -0,0 +1,10 @@
+#Diccionarios pueden almacenar tuplas, varialebls,  listas, diccionarios, strings, etc.
+#son mutables
+#no tienen indice, tienen llaves
+
+# diccionario = {}
+# diccionario2 = dict()
+
+diccionario = {'edad':33}
+
+print(diccionario)
--- tuplas.py
+++ tuplas.py
@@ -2,14 +2,37 @@
 #son mas rapidas que las listas
 #pueden contener cualqueir tipo de datos, incluso listas y otras tuplas
 
-lista = ['a','b','c']
+# lista = ['a','b','c']
 
-miTupla = (1,2,3,4,5,lista)
+# miTupla = (1,2,3,4,5,lista)
 
-tuplaDos = ('perro','gato','canario',miTupla)
+# tuplaDos = ('perro','gato','canario',miTupla)
 
-print(tuplaDos)
+# print(tuplaDos)
 
-lista.append('casa')
+# lista.append('casa')
 
-print(tuplaDos)
+# print(tuplaDos)
+
+# colores = ('rojo', 'verde', 'amarillo','violeta', 'marron')
+
+# uno, dos, tres,*cuatro = colores
+
+# print(uno)
+# print(dos)
+# print(tres)
+# print (cuatro)
+
+
+lista = [11,12,13,14,15]
+miTupla = (1,2,3,4,5,6,7,8,9,10)
+
+combineta = zip(lista,miTupla)
+
+nuevaTupla = tuple(combineta)
+
+print(nuevaTupla)
+
+listaNueva = list(miTupla)
+
+print(listaNueva)
