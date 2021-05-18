9f7534e35599c4ae49a3b63d683eddf17e595851
dario8a <dario.martin.8a@gmail.com>
2021-05-17 21:23:32 -0300

    minor chages in ej02 and ej05

--- ejercicios-varios/ej02.py
+++ ejercicios-varios/ej02.py
@@ -2,13 +2,15 @@
 de dividir el cuadrado del mayor de ellos y el cuadrado 
 del menor de ellos. Si los números son iguales escribir un
 mensaje."""
+def funcion_cuadrado_mayor():
+    numero_1 = int(input('ingrese el primer número: '))
+    numero_2 = int(input('ingrese el segundo número: '))
 
-numero_1 = int(input('ingrese el primer número: '))
-numero_2 = int(input('ingrese el segundo número: '))
+    if numero_1 > numero_2 :
+        print('el resultado de dividir el cuadro del 1er numero en el cuadrado del 2do numero es: ', (numero_1*numero_1) / (numero_2*numero_2))
+    elif numero_2 > numero_1 :
+        print('el resultado de dividir el cuadro del 2do numero en el cuadrado del 1er numero es: ', (numero_2*numero_2) / (numero_1*numero_1))
+    else:
+        print('los numeros son iguales')
 
-if numero_1 > numero_2 :
-    print('el resultado de dividir el cuadro del 1er numero en el cuadrado del 2do numero es: ', (numero_1*numero_1) / (numero_2*numero_2))
-elif numero_2 > numero_1 :
-    print('el resultado de dividir el cuadro del 2do numero en el cuadrado del 1er numero es: ', (numero_2*numero_2) / (numero_1*numero_1))
-else:
-    print('los numeros son iguales')
+funcion_cuadrado_mayor()
--- ejercicios-varios/ej05.py
+++ ejercicios-varios/ej05.py
@@ -2,7 +2,6 @@
 el segundo es igual al tercero. Si se cumple, escribir 
 "La suma es igual al tercero", si no, escribir 
 "La suma es distinta al tercero".'''
-
 numero_1 = int(input('ingrese el primer número: '))
 numero_2 = int(input('ingrese el segundo número: '))
 numero_3 = int(input('ingrese el tercer número: '))
@@ -11,3 +10,4 @@ if numero_1+numero_2 == numero_3 :
     print('La suma es IGUAL al tercer numero')
 else:
     print('La suma es DISTINTA del tercer numero')
+
