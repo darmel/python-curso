701c4d2813e6d20ef0ff3ce95d1daeb00be4aba6
dario8a <dario.martin.8a@gmail.com>
2021-05-28 13:02:40 -0300

    listo ejercicio de funciones

--- ejercicios-varios/funciones-incluit/principal.py
+++ ejercicios-varios/funciones-incluit/principal.py
@@ -9,21 +9,22 @@ print(far_to_cel())'''
 opcion = 1
 
 def menu():
-    opcion=int(input('''
+    op=int(input('''
     Ingrese una opcion:
-    1
-    2
-    3
-    4
+    1 - Menor de Diez
+    2 - Es la suma de los 2 primeros igual al 3ero?
+    3 - Division del cuadro del mayor en el cuadrado del menor
+    4 - Conversor Fherenheit a Centigrados
     0 - para salir
     '''))
+    print('opción ingresada: ', op)
+    return op
 
-#menu()
 
 while opcion != 0 :
-    menu()
+    opcion=menu()
     if opcion==1:
-        menor_diez
+        menor_diez()
     elif opcion==2:
         numero_1 = int(input('ingrese el primer número: '))
         numero_2 = int(input('ingrese el segundo número: '))
@@ -32,8 +33,11 @@ while opcion != 0 :
     elif opcion==3:
         numero_1 = int(input('ingrese el primer número: '))
         numero_2 = int(input('ingrese el segundo número: '))
-        cuadrado_mayor(numero_1,numero_2)
+        print('''la division del cuadrado del mayor
+en el cuadrado del menor es: ''', cuadrado_mayor(numero_1,numero_2) )
     elif opcion==4:
         print('La temperatura en grados Celcius es: ', far_to_cel())
+    elif opcion==0:
+        print('Muchas gracias por utilizarme')
     else:
         print('Opcion no disponible')
