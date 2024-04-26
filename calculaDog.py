9b1893f0ed0d9528f39daf045d89372dec29182a
Dario OCHOA <dario.martin.8a@gmail.com>
2024-04-26 11:35:26 -0300

    clean up

--- calculaDog.py
+++
@@ -1,7 +0,0 @@
-#Calcula la edad de tu perro   
-multiplica = 7
-edad = int(input('Ingrese la edad de su perro: '))
-
-edadPerro = edad * multiplica
-
-print('La edad de tu perro en años perrunos es: ', edadPerro)
